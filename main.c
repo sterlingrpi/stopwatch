1cfaa85f8eeb104f5f89b4fc6a3fe0ee1a8274c2
sterlingrpi <sterling.nesbitt@gmail.com>
2019-10-28 11:13:26 -0400

    Update main.c
    added indents for readability

--- main.c
+++ main.c
@@ -20,23 +20,26 @@ int button=0; //input
 char a [10]={ 0x7E,0x30,0x6D,0x79,0x33,0x5B,0x5F,0x70,0x7F,0x7B};   // Array setup to save the values I will uses on my outputs to display numbers on the seven segment   
 SysInit();    
 while (1)  {
-button=PORTA;
-if (button != last){    // start running the counter. 
-      
- for (d = min; d < maxsec; d++) { 
-PORTB= a[d];
- s=0;
-   for (s = min; s < max; s++) { 
-   PTT=a[s] ;
-   m=0;
-     for (m = min; m < max; m++) {
-     PTAD=a[m];
-     delay(); 
-     button=last;  
-}
-}
-}
-}
+      button=PORTA;
+      if (button != last)
+      {    // start running the counter. 
+            for (d = min; d < maxsec; d++) 
+            {
+                  PORTB= a[d];
+                  s=0;
+                  for (s = min; s < max; s++) 
+                  { 
+                        PTT=a[s] ;
+                        m=0;
+                        for (m = min; m < max; m++) 
+                        {
+                              PTAD=a[m];
+                              delay(); 
+                              button=last;  
+                        }
+                   }
+            }
+      }
 }
 
  if (button==2 || d==6){  // reset routine. 
